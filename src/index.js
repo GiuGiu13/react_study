ff99ba4a460d573cd4b05f36d96eedd866e0f1e6
Giusy <luminal@ymail.com>
2019-09-18 13:33:33 +0100

    axios default config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+//setting default configuration with axios
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 // var myInterceptor = axios.interceptors.request.use(function () {/*...*/});
 // axios.interceptors.request.eject(myInterceptor);
