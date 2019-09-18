b74a33fec88cfef5d0b76c823758c0d0a76c1df8
Giusy <luminal@ymail.com>
2019-09-18 13:28:04 +0100

    interceptors axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+
+// var myInterceptor = axios.interceptors.request.use(function () {/*...*/});
+// axios.interceptors.request.eject(myInterceptor);
+axios.interceptors.request.use(response => {
+    console.log(response);
+    //edit request config
+    return response;
+}, error =>{
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(request => {
+    console.log(request);
+    //edit request config
+    return request;
+}, error =>{
+    console.log(error);
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
