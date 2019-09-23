081d1249ed3f3315be19bd97679191f46dc88d0d
Giusy <luminal@ymail.com>
2019-09-23 12:31:30 +0100

    nested rout show the right post

--- src/containers/FullPost/FullPost.js
+++ src/containers/FullPost/FullPost.js
@@ -8,9 +8,16 @@ class FullPost extends Component {
         loadedPost: null
     }
     componentDidMount (){
+        this.loadData();
         
+    }
+
+    componentDidUpdate(){
+        this.loadData();
+    }
+    loadData(){
         if(this.props.match.params.id){
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id)){
                 axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         // console.log(response)
@@ -20,14 +27,14 @@ class FullPost extends Component {
         }
     }
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response)
             });
     }
     render () {
         let post = <p style={{textAlign: 'Center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign: 'Center'}}>Loading....</p>;
         }
         
