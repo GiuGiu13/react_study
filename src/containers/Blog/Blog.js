952d7d9394af972140aaf03470a5f20ac26c0793
Giusy <luminal@ymail.com>
2019-09-23 11:55:00 +0100

    swithc component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import Posts from '../Posts/Posts';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 import NewPost from '../NewPost/NewPost';
 import FullPost from '../FullPost/FullPost'
 
@@ -42,11 +42,15 @@ class Blog extends Component {
                 </header>
                 {/* <Route path='/' exact render={() => <h1>home</h1>} />
                 <Route path='/'  render={() => <h1>home2</h1>} /> */}
+
                 <Route path='/' exact component={Posts} />
-                <Route path='/new-post' component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
+                <Switch>
+                      {/* the order of the route is very important*/}
+                    
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
                
-                
+                </Switch>
 
                 {/* <section>
                     <FullPost id={this.state.selectedPostId}/>
