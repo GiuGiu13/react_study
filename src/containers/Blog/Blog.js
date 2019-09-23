d314fe91ed80869d303afd3c525937d2dc0ca08b
Giusy <luminal@ymail.com>
2019-09-23 11:03:15 +0100

    using link component to don't reload the page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import Posts from '../Posts/Posts';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
+import NewPost from '../NewPost/NewPost'
 
 import './Blog.css';
 
@@ -12,14 +13,19 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash:'#submit',
+                                search:'?quick-submit=true'
+                                }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path='/' exact render={() => <h1>home</h1>} />
                 <Route path='/'  render={() => <h1>home2</h1>} /> */}
                 <Route path='/' exact component={Posts} />
+                <Route path='/new-post' component={NewPost} />
                 
 
                 {/* <section>
